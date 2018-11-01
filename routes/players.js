82ff1f1578a1740775720eb47ae820f311b20240
Allan James Lapid <ajLapid718@gmail.com>
2018-11-01 02:47:01 -0400

    Query for all players and query for a single player

--- routes/players.js
+++ routes/players.js
@@ -2,9 +2,17 @@ const express = require('express');
 const router = express.Router();
 const { Player } = require('../database/models');
 
+// Find all the players;
 router.get('/', function(req, res, next) {
   Player.findAll()
     .then(players => res.json(players))
+    .catch(next)
+});
+
+// Find a specific, individual player;
+router.get('/:id', function(req, res, next) {
+  Player.findById(req.params.id) // findById() is deprecated, so findByPk (primary key) is more preferred moving forward;
+    .then(player => res.json(player))
     .catch(next)
 });
 
