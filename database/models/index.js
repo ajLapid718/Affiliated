941d36fd9367a81c649fcd4953fa37483699a44e
Allan James Lapid <ajLapid718@gmail.com>
2018-11-01 04:09:52 -0400

    Add documentation regarding which methods become available via associations

--- database/models/index.js
+++ database/models/index.js
@@ -7,11 +7,56 @@ const Team = require('./team');
 // Associations;
 // Source.association(Target);
 
+// O:M;
 Team.hasMany(Player); // A one-to-many relationship that adds the column titled "teamId" to the table of players;
 Player.belongsTo(Team); // A one-to-one relationship that adds the column titled "teamId" to the table of players;
 
+// O:O;
 Team.hasOne(Coach); // A one-to-one relationship that adds the column titled "teamId" to the table of coaches;
 Coach.belongsTo(Team); // A one-to-one relationship that adds the column titled "teamId" to the table of coaches;
+
+/*
+
+The hasOne() association provides the following methods for the instance of the Source;
+
+Using our specific example, hasOne() makes the following methods available and scenarios possible;
+
+Team.hasOne(Coach);
+
+Team.getCoach;
+Team.setCoach;
+Team.addCoach;
+Team.createCoach;
+Team.removeCoach;
+Team.hasCoach;
+
+Similarly, the hasMany() association provides the following methods for the instance of the Source:
+
+Team.hasMany(Player);
+
+Team.getPlayers;
+Team.setPlayers;
+Team.addPlayer;
+Team.createPlayer;
+Team.removePlayer;
+Team.hasPlayer;
+Team.hasPlayers;
+
+Similarly, the belongsTo() association provides the following methods for the instance of the Source:
+
+Player.belongsTo(Team);
+
+Player.getTeam;
+Player.setTeam;
+Player.createTeam;
+
+Coach.belongsTo(Team);
+
+Coach.getTeam;
+Coach.setTeam;
+Coach.createTeam;
+
+*/
 
 module.exports = {
   Player,
