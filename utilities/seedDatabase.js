897ef7296dfa7d2f09d6b54bb93d9dc376f75255
Allan James Lapid <ajLapid718@gmail.com>
2018-11-01 02:15:39 -0400

    Iterate through data and create rows in the tables

--- utilities/seedDatabase.js
+++ utilities/seedDatabase.js
@@ -4,12 +4,24 @@ const coaches = require('../data/coaches');
 const teams = require('../data/teams');
 
 const populatePlayersTable = async (players) => {
+  for (let i = 0; i < players.length; i++) {
+    let currentPlayer = players[i];
+    await Player.create(currentPlayer);
+  }
 }
 
 const populateCoachesTable = async (coaches) => {
+  for (let i = 0; i < coaches.length; i++) {
+    let currentCoach = coaches[i];
+    await Coach.create(currentCoach);
+  }
 }
 
 const populateTeamsTable = async (teams) => {
+  for (let i = 0; i < teams.length; i++) {
+    let currentTeam = teams[i];
+    await Team.create(currentTeam);
+  }
 }
 
 const seedDatabase = async () => {
