2768752f5b7e5efb4bf6bc1dc7b25f37596045fc
Allan James Lapid <ajLapid718@gmail.com>
2018-11-16 04:49:51 -0500

    Add documentation and switch order of GET routes for teams

--- routes/teams.js
+++ routes/teams.js
@@ -9,30 +9,7 @@ router.get('/', function(req, res, next) {
     .catch(next)
 });
 
-
-router.get('/:id/coach', async (req, res, next) => {
-  let foundTeam;
-
-  try {
-    foundTeam = await Team.findOne({where: { id: req.params.id }});
-  }
-  catch (err) {
-    next(err);
-  }
-
-  let coachOfTeam;
-
-  try {
-    coachOfTeam = await foundTeam.getCoach();
-  }
-  catch (err) {
-    next(err);
-  }
-
-  res.status(200).json(coachOfTeam);
-});
-
-// Find a particular team and eager load all players who belong on/share the same team;
+// Find a particular team and "eager load" all players who belong on/share the same team;
 router.get('/:id/players', async function(req, res, next) {
   let foundTeam;
 
@@ -53,6 +30,29 @@ router.get('/:id/players', async function(req, res, next) {
   }
 
   res.status(200).json(playersOfTeam);
+});
+
+// Find a particular team and respond with the coach of that particular team;
+router.get('/:id/coach', async (req, res, next) => {
+  let foundTeam;
+
+  try {
+    foundTeam = await Team.findOne({where: { id: req.params.id }});
+  }
+  catch (err) {
+    next(err);
+  }
+
+  let coachOfTeam;
+
+  try {
+    coachOfTeam = await foundTeam.getCoach();
+  }
+  catch (err) {
+    next(err);
+  }
+
+  res.status(200).json(coachOfTeam);
 });
 
 // Export our router, so that it can be imported to construct our apiRouter;
