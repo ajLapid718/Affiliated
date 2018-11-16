c0e37589898d274260df5338e9ebf11913ca354c
Allan James Lapid <ajLapid718@gmail.com>
2018-11-15 23:59:30 -0500

    Add dummy data of trainers

---
+++ data/trainers.js
@@ -0,0 +1,13 @@
+// Trainers who train NBA players;
+const trainers = [
+  {
+    "firstName": "Tyler",
+    "lastName": "Relph"
+  },
+  {
+    "firstName": "Jordan",
+    "lastName": "Lawley"
+  }
+];
+
+module.exports = trainers;
