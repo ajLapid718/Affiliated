0869633bc88f1151c074c370dd2ca83a452cb19f
Allan James Lapid <ajLapid718@gmail.com>
2018-11-20 15:29:04 -0500

    Set up seed data for Games

---
+++ data/games.js
@@ -0,0 +1,41 @@
+// Seed data to populate the table of games;
+const games = [
+  {
+    "dateOfGame": "11/11/2018",
+    "location": "Boston",
+    "homeTeamScore": "89",
+    "awayTeamScore": "82"
+  },
+  {
+    "dateOfGame": "11/11/2018",
+    "location": "New York",
+    "homeTeamScore": "103",
+    "awayTeamScore": "79"
+  },
+  {
+    "dateOfGame": "11/11/2018",
+    "location": "New York",
+    "homeTeamScore": "76",
+    "awayTeamScore": "71"
+  },
+  {
+    "dateOfGame": "11/17/2018",
+    "location": "New York",
+    "homeTeamScore": "77",
+    "awayTeamScore": "93"
+  },
+  {
+    "dateOfGame": "11/18/2018",
+    "location": "Boston",
+    "homeTeamScore": "80",
+    "awayTeamScore": "90"
+  },
+  {
+    "dateOfGame": "11/19/2018",
+    "location": "Oakland",
+    "homeTeamScore": "123",
+    "awayTeamScore": "109"
+  }
+];
+
+module.exports = games;
