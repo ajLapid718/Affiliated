5826900d5f6a57cd32b239de98cca30670074ac6
Allan James Lapid <ajLapid718@gmail.com>
2018-11-21 18:08:25 -0500

    Convert score values from strings to integers

--- data/games.js
+++ data/games.js
@@ -3,38 +3,38 @@ const games = [
   {
     "dateOfGame": "11/11/2018",
     "location": "Boston",
-    "homeTeamScore": "89",
-    "awayTeamScore": "82"
+    "homeTeamScore": 89,
+    "awayTeamScore": 82
   },
   {
     "dateOfGame": "11/11/2018",
     "location": "New York",
-    "homeTeamScore": "103",
-    "awayTeamScore": "79"
+    "homeTeamScore": 103,
+    "awayTeamScore": 79
   },
   {
     "dateOfGame": "11/11/2018",
     "location": "New York",
-    "homeTeamScore": "76",
-    "awayTeamScore": "71"
+    "homeTeamScore": 76,
+    "awayTeamScore": 71
   },
   {
     "dateOfGame": "11/17/2018",
     "location": "New York",
-    "homeTeamScore": "77",
-    "awayTeamScore": "93"
+    "homeTeamScore": 77,
+    "awayTeamScore": 93
   },
   {
     "dateOfGame": "11/18/2018",
     "location": "Boston",
-    "homeTeamScore": "80",
-    "awayTeamScore": "90"
+    "homeTeamScore": 80,
+    "awayTeamScore": 90
   },
   {
     "dateOfGame": "11/19/2018",
     "location": "Oakland",
-    "homeTeamScore": "123",
-    "awayTeamScore": "109"
+    "homeTeamScore": 123,
+    "awayTeamScore": 109
   }
 ];
 
