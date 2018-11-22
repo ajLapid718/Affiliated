61e5e7e37e8b97c9fc4493a3d8baaacacda9871b
Allan James Lapid <ajLapid718@gmail.com>
2018-11-22 00:06:09 -0500

    Add todos regarding eager loading the games of a team

--- routes/teams.js
+++ routes/teams.js
@@ -57,6 +57,7 @@ router.get('/:id/coach', async (req, res, next) => {
 
 // Find all of the games of a particular team;
 router.get('/:id/games', async (req, res, next) => {
+  // TODO: Declare a more semantic variable name, add error handling, and consider querying the Game table and finding which games have the particular teamId as either the homeTeamId OR (Sequelize operator) the awayTeamId;
   let huh = await Team.findOne({
     where: {
       id: req.params.id
